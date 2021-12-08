eb025a6493f15010ced2983aecb90c2ae53c9a82
Mario <macedo@medinfo.uni-kiel.de>
2021-12-08 13:36:57 +0100

    Add new disease files and update ExportResourceMappingConfig - Lung Diseases

---
+++ src/main/groovy/projects/gecco/crf/conditionChronicLungDisease_lungenfibrose.groovy
@@ -0,0 +1,162 @@
+package projects.gecco.crf
+
+import ca.uhn.fhir.model.api.TemporalPrecisionEnum
+import de.kairos.fhir.centraxx.metamodel.CatalogEntry
+import de.kairos.fhir.centraxx.metamodel.CrfItem
+import de.kairos.fhir.centraxx.metamodel.CrfTemplateField
+import de.kairos.fhir.centraxx.metamodel.LaborValue
+
+import static de.kairos.fhir.centraxx.metamodel.RootEntities.studyVisitItem
+
+/**
+ * Old version
+ * Represented by a CXX StudyVisitItem
+ * Specified by https://simplifier.net/forschungsnetzcovid-19/chroniclungdiseases
+ * @author Lukas Reinert, Mike Wähnert
+ * @since KAIROS-FHIR-DSL.v.1.8.0, CXX.v.3.18.1
+ *
+ * New version
+ * @author Mário Macedo
+ */
+condition {
+  final def studyCode = context.source[studyVisitItem().studyMember().study().code()]
+  if (studyCode != "GECCO FINAL") {
+    return //no export
+  }
+  final def crfName = context.source[studyVisitItem().template().crfTemplate().name()]
+  final def studyVisitStatus = context.source[studyVisitItem().status()]
+  if (crfName != "SarsCov2_ANAMNESE / RISIKOFAKTOREN" || studyVisitStatus == "OPEN") {
+    return //no export
+  }
+
+  final def crfItemLung = context.source[studyVisitItem().crf().items()].find {
+    "COV_GECCO_LUNGENERKRANKUNG_LUNGENFIBROSE" == it[CrfItem.TEMPLATE]?.getAt(CrfTemplateField.LABOR_VALUE)?.getAt(LaborValue.CODE)
+  }
+
+  if (crfItemLung[CrfItem.CATALOG_ENTRY_VALUE] != []) {
+    id = "Condition/ChronicLungDisease-Lungenfibrose-" + context.source[studyVisitItem().crf().id()]
+
+    meta {
+      source = "https://fhir.centraxx.de"
+      profile "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/chronic-lung-diseases"
+    }
+
+    crfItemLung[CrfItem.CATALOG_ENTRY_VALUE]?.each { final item ->
+      final def VERcode = matchResponseToVerificationStatus(item[CatalogEntry.CODE] as String)
+      final def VERcode_JA = matchResponseToVerificationStatus("COV_JA")
+      final def VERcode_NEIN = matchResponseToVerificationStatus("COV_NEIN")
+      final def VERcode_UNBEKANNT = matchResponseToVerificationStatus("COV_UNBEKANNT")
+
+      // Disease confirmed Present
+      if (VERcode == VERcode_JA) {
+
+        verificationStatus {
+          coding {
+            system = "http://terminology.hl7.org/CodeSystem/condition-ver-status"
+            code = matchResponseToVerificationStatusHL7(item[CatalogEntry.CODE] as String)
+          }
+          coding {
+            system = "http://snomed.info/sct"
+            code = VERcode
+          }
+        }
+
+        // Disease confirmed Absense
+      } else if (VERcode == VERcode_NEIN) {
+        verificationStatus {
+          coding {
+            system = "http://terminology.hl7.org/CodeSystem/condition-ver-status"
+            code = matchResponseToVerificationStatusHL7(item[CatalogEntry.CODE] as String)
+          }
+          coding {
+            system = "http://snomed.info/sct"
+            code = VERcode
+          }
+        }
+
+        // Disease presence unknown
+      } else if (VERcode == VERcode_UNBEKANNT) {
+        modifierExtension {
+          url = "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/uncertainty-of-presence"
+          valueCodeableConcept {
+            coding {
+              system = "http://snomed.info/sct"
+              code = VERcode_UNBEKANNT
+              display = "Unknown (qualifier value)"
+            }
+            text =  "Presence of condition is unknown."
+          }
+        }
+      }
+    }
+
+    category {
+      coding {
+        system = "http://snomed.info/sct"
+        code = "418112009"
+        display = "Pulmonary medicine"
+      }
+    }
+
+    code {
+      final def ICDcode = "J84.1"
+      if (ICDcode != "") {
+        coding {
+          system = "http://fhir.de/CodeSystem/dimdi/icd-10-gm"
+          version = "2020"
+          code = ICDcode
+          display = "Fibrosis of lung"
+        }
+      }
+
+      final def SNOMEDcode = "51615001"
+      if (SNOMEDcode != "") {
+        coding {
+          system = "http://snomed.info/sct"
+          code = SNOMEDcode
+          display = "Fibrosis of lung"
+        }
+      }
+    }
+
+    subject {
+      reference = "Patient/Patient-" + context.source[studyVisitItem().studyMember().patientContainer().id()]
+    }
+
+    recordedDate {
+      date = normalizeDate(crfItemLung[CrfItem.CREATIONDATE] as String)
+      precision = TemporalPrecisionEnum.DAY.toString()
+    }
+
+  }
+}
+
+static String normalizeDate(final String dateTimeString) {
+  return dateTimeString != null ? dateTimeString.substring(0, 10) : null
+}
+
+static String matchResponseToVerificationStatus(final String resp) {
+  switch (resp) {
+    case null:
+      return null
+    case ("COV_UNBEKANNT"):
+      return "261665006"
+    case ("COV_NEIN"):
+      return "410594000"
+    //"COV_JA"
+    default: "410605003"
+  }
+}
+
+static String matchResponseToVerificationStatusHL7(final String resp) {
+  switch (resp) {
+    case null:
+      return null
+    case ("COV_UNBEKANNT"):
+      return "unconfirmed"
+    case ("COV_NEIN"):
+      return "refuted"
+    //"COV_JA"
+    default: "confirmed"
+  }
+}
