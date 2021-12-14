1b007939ad170b3f925eaa89443df55aa4b5070a
Mario <macedo@medinfo.uni-kiel.de>
2021-12-14 10:21:54 +0100

    Add therapeutic intent

--- src/main/groovy/projects/gecco/crf/medicationStatementAnticoagulants.groovy
+++ src/main/groovy/projects/gecco/crf/medicationStatementAnticoagulants.groovy
@@ -24,48 +24,70 @@ medicationStatement {
   if (crfName != "SarsCov2_MEDIKATION" || studyVisitStatus == "OPEN") {
     return //no export
   }
-  final def crfItemThera = context.source[studyVisitItem().crf().items()].find {
+  final def crfItemATC = context.source[studyVisitItem().crf().items()].find {
     "COV_GECCO_ANTIKOAGULATION" == it[CrfItem.TEMPLATE]?.getAt(CrfTemplateField.LABOR_VALUE)?.getAt(LaborValue.CODE)
   }
-  if (!crfItemThera) {
+  if (!crfItemATC) {
     return
   }
-  if (crfItemThera[CrfItem.CATALOG_ENTRY_VALUE] != []) {
-    id = "MedicationStatement/AntiCoagulation-" + context.source[studyVisitItem().id()]
+  if (crfItemATC[CrfItem.CATALOG_ENTRY_VALUE] == []) {
+    return
+  }
+  final def crfItemThera = context.source[studyVisitItem().crf().items()].find {
+    "COV_GECCO_ANTIKOAGULATION_ABSICHT" == it[CrfItem.TEMPLATE]?.getAt(CrfTemplateField.LABOR_VALUE)?.getAt(LaborValue.CODE)
+  }
 
-    meta {
-      source = "https://fhir.centraxx.de"
-      profile "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/pharmacological-therapy-anticoagulants"
-    }
+  id = "MedicationStatement/AntiCoagulation-" + context.source[studyVisitItem().id()]
 
+  meta {
+    source = "https://fhir.centraxx.de"
+    profile "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/pharmacological-therapy-anticoagulants"
+  }
 
-    crfItemThera[CrfItem.CATALOG_ENTRY_VALUE]?.each { final item ->
-      final def STATUScode = matchResponseToSTATUS(item[CatalogEntry.CODE] as String)
-      if (STATUScode) {
-        status = STATUScode
-      }
-    }
+  crfItemATC[CrfItem.CATALOG_ENTRY_VALUE]?.each { final item ->
+    status = matchResponseToSTATUS(item[CatalogEntry.CODE] as String)
+  }
 
-    medication {
-      medicationCodeableConcept {
-        crfItemThera[CrfItem.CATALOG_ENTRY_VALUE]?.each { final item ->
-          final def ATCcode = matchResponseToATC(item[CatalogEntry.CODE] as String)
-          if (ATCcode) {
-            coding {
-              system = "http://fhir.de/CodeSystem/dimdi/atc"
-              code = ATCcode
-            }
-          }
+  medicationCodeableConcept {
+    crfItemATC[CrfItem.CATALOG_ENTRY_VALUE]?.each { final item ->
+      final def ATCinfo = getAnticoagulantATCInfo(item[CatalogEntry.CODE] as String)
+
+      if (ATCinfo[0]) {
+        coding{
+          system = "http://snomed.info/sct"
+          code = "81839001"
+          display = "Medicinal product acting as anticoagulant agent (product)"
+        }
+
+        coding {
+          system = "http://fhir.de/CodeSystem/dimdi/atc"
+          code = ATCinfo[0]
+          display = ATCinfo[1]
         }
       }
     }
+  }
 
-    subject {
-      reference = "Patient/Patient-" + context.source[studyVisitItem().studyMember().patientContainer().id()]
-    }
-    effectiveDateTime {
-      date = normalizeDate(context.source[studyVisitItem().crf().creationDate()] as String)
-      precision = TemporalPrecisionEnum.SECOND.toString()
+  subject {
+    reference = "Patient/Patient-" + context.source[studyVisitItem().studyMember().patientContainer().id()]
+  }
+
+  effectiveDateTime {
+    date = normalizeDate(context.source[studyVisitItem().crf().creationDate()] as String)
+    precision = TemporalPrecisionEnum.SECOND.toString()
+  }
+
+  reasonCode{
+    crfItemThera[CrfItem.CATALOG_ENTRY_VALUE]?.each { final item ->
+      final def theraIntentInfo = getTherapeuticIntentSnomedInfo(item[CatalogEntry.CODE] as String)
+
+      if (theraIntentInfo[0]) {
+        coding {
+          system = "http://snomed.info/sct"
+          code = theraIntentInfo[0]
+          display = theraIntentInfo[1]
+        }
+      }
     }
   }
 }
@@ -74,24 +96,33 @@ static String normalizeDate(final String dateTimeString) {
   return dateTimeString != null ? dateTimeString.substring(0, 19) : null
 }
 
-
-static String matchResponseToATC(final String resp) {
+static String[] getAnticoagulantATCInfo(final String resp) {
   switch (resp) {
     case ("COV_UNFRAK_HEPARIN"):
-      return "B01AB01"
+      return ["B01AB01", "Heparin"]
     case ("COV_NIEDERMOL_HEPARIN"):
-      return "B01AB08" //?
+      return ["B01AB08", "Reviparin"] //?
     case ("COV_ARGATROBAN"):
-      return "B01AE03"
+      return ["B01AE03", "Argatroban"]
     case ("COV_PLAETTCHENAGGRHEMMER"):
-      return "B01AC"
+      return ["B01AC", "Thrombozytenaggregationshemmer, exkl. Heparin"]
     case ("COV_DANAPAROID"):
-      return "B01AB09"
+      return ["B01AB09", "Danaparoid"]
     case ("COV_PHENPROCOUMON"):
-      return "B01AA04"
+      return ["B01AA04", "Phenprocoumon"]
     case ("COV_DOAK"):
-      return "B01AE"
-    default: null
+      return ["B01AE", "Direkte Thrombininhibitoren"]
+    default: [null, null]
+  }
+}
+
+static String[] getTherapeuticIntentSnomedInfo(final String resp) {
+  switch (resp) {
+    case ("COV_GECCO_THERAPEUTISCH_ABSICHT_PROPHYLAKTISCH"):
+      return ["360271000", "Prophylaxis - procedure intent (qualifier value)"]
+    case ("COV_GECCO_THERAPEUTISCH_ABSICHT_THERAPEUTISCH"):
+      return ["373808002", "Curative - procedure intent (qualifier value)"]
+    default: [null, null]
   }
 }
 
@@ -99,6 +130,6 @@ static String matchResponseToSTATUS(final String resp) {
   switch (resp) {
     case ("COV_NEIN"):
       return "not-taken"
-    default: "unknown"
+    default: "active"
   }
 }
