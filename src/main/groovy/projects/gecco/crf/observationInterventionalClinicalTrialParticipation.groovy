fbecd20f02cefa7d332453a5b63e06705730408b
Mario <macedo@medinfo.uni-kiel.de>
2021-12-14 17:02:56 +0100

    Add eudraCT and NCT number in study

--- src/main/groovy/projects/gecco/crf/observationInterventionalClinicalTrialParticipation.groovy
+++ src/main/groovy/projects/gecco/crf/observationInterventionalClinicalTrialParticipation.groovy
@@ -6,6 +6,7 @@ import de.kairos.fhir.centraxx.metamodel.CrfItem
 import de.kairos.fhir.centraxx.metamodel.CrfTemplateField
 import de.kairos.fhir.centraxx.metamodel.LaborValue
 import org.hl7.fhir.r4.model.Observation
+import org.hl7.fhir.r4.model.StringType
 
 import static de.kairos.fhir.centraxx.metamodel.RootEntities.studyVisitItem
 
@@ -30,92 +31,115 @@ observation {
     return //no export
   }
   final def crfItemStudy = context.source[studyVisitItem().crf().items()].find {
-    "COV_GECCO_STUDIENEINSCHLUSS" == it[CrfItem.TEMPLATE]?.getAt(CrfTemplateField.LABOR_VALUE)?.getAt(LaborValue.CODE)
+    "COV_GECCO_OUTCOME_INTERVENTIONELL_STUDIE" == it[CrfItem.TEMPLATE]?.getAt(CrfTemplateField.LABOR_VALUE)?.getAt(LaborValue.CODE)
   }
-  if (!crfItemStudy) {
+  if (!crfItemStudy || crfItemStudy[CrfItem.CATALOG_ENTRY_VALUE] == []) {
     return
   }
-  if (crfItemStudy[CrfItem.CATALOG_ENTRY_VALUE] != []) {
-    id = "Observation/InterventionalTrialParticipation-" + context.source[studyVisitItem().id()]
 
-    meta {
-      source = "https://fhir.centraxx.de"
-      profile "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/interventional-clinical-trial-participation"
+  final String answerCODE = crfItemStudy[CrfItem.CATALOG_ENTRY_VALUE][0][CatalogEntry.CODE]
+
+  id = "Observation/InterventionalTrialParticipation-" + context.source[studyVisitItem().id()]
+
+  meta {
+    source = "https://fhir.centraxx.de"
+    profile "https://www.netzwerk-universitaetsmedizin.de/fhir/StructureDefinition/interventional-clinical-trial-participation"
+  }
+
+  // TODO: What about identifier ?
+
+  status = Observation.ObservationStatus.UNKNOWN
+
+  category {
+    coding {
+      system = "http://terminology.hl7.org/CodeSystem/observation-category"
+      code = "survey"
     }
+  }
 
-    status = Observation.ObservationStatus.UNKNOWN
+  code {
+    coding {
+      system = "https://www.netzwerk-universitaetsmedizin.de/fhir/CodeSystem/ecrf-parameter-codes"
+      code = "03"
+      display = "Participation in interventional clinical trials"
+    }
+    text = "Has the patient participated in one or more interventional clinical trials?"
+  }
 
-    category {
+  subject {
+    reference = "Patient/Patient-" + context.source[studyVisitItem().studyMember().patientContainer().id()]
+  }
+
+  effectiveDateTime {
+    date = normalizeDate(context.source[studyVisitItem().crf().creationDate()] as String)
+    precision = TemporalPrecisionEnum.SECOND.toString()
+  }
+
+  valueCodeableConcept {
+    final def SNOMEDfields = getSnomedFields(answerCODE)
+    if (SNOMEDfields[0]) {
       coding {
-        system = "http://terminology.hl7.org/CodeSystem/observation-category"
-        code = "survey"
+        system = "http://snomed.info/sct"
+        code = SNOMEDfields[0]
+        display = SNOMEDfields[1]
       }
     }
 
-    code {
-      coding {
-        system = "https://www.netzwerk-universitaetsmedizin.de/fhir/CodeSystem/ecrf-parameter-codes"
-        code = "03"
-      }
-    }
+  }
 
-    subject {
-      reference = "Patient/Patient-" + context.source[studyVisitItem().studyMember().patientContainer().id()]
-    }
+  // EudraCT Number
+  final def crfItemEudraCT = context.source[studyVisitItem().crf().items()].find {
+    "COV_GECCO_STUDIE_EUDRACT_NUMMER" == it[CrfItem.TEMPLATE]?.getAt(CrfTemplateField.LABOR_VALUE)?.getAt(LaborValue.CODE)
+  }
 
-    effectiveDateTime {
-      date = normalizeDate(context.source[studyVisitItem().crf().creationDate()] as String)
-      precision = TemporalPrecisionEnum.SECOND.toString()
-    }
-
-    valueCodeableConcept {
-      crfItemStudy[CrfItem.CATALOG_ENTRY_VALUE]?.each { final item ->
-        final def SNOMEDcode = mapInclusionSNOMED(item[CatalogEntry.CODE] as String)
-        if (SNOMEDcode) {
-          coding {
-            system = "http://snomed.info/sct"
-            code = SNOMEDcode
-          }
-        }
-      }
-    }
-    /*
+  if (crfItemEudraCT[CrfItem.STRING_VALUE]) {
     component {
       code {
         coding {
           system = "https://www.netzwerk-universitaetsmedizin.de/fhir/CodeSystem/ecrf-parameter-codes"
           code = "04"
+          display = "EudraCT Number"
         }
+        text = "EudraCT (European Union Drug Regulating Authorities Clinical Trials) registration number"
       }
+      valueString = new StringType(crfItemEudraCT[CrfItem.STRING_VALUE] as String)
     }
+  }
+
+  // NCT Number
+  final def crfItemNCT = context.source[studyVisitItem().crf().items()].find {
+    "COV_GECCO_STUDIE_NCT_NUMMER" == it[CrfItem.TEMPLATE]?.getAt(CrfTemplateField.LABOR_VALUE)?.getAt(LaborValue.CODE)
+  }
+  if (crfItemNCT[CrfItem.STRING_VALUE]) {
     component {
       code {
         coding {
           system = "https://www.netzwerk-universitaetsmedizin.de/fhir/CodeSystem/ecrf-parameter-codes"
           code = "05"
+          display = "NCT Number"
         }
+        text = "NCT number"
       }
+      valueString = new StringType(crfItemNCT[CrfItem.STRING_VALUE] as String)
     }
-    */
-
-
   }
 }
-
 
 static String normalizeDate(final String dateTimeString) {
   return dateTimeString != null ? dateTimeString.substring(0, 19) : null
 }
 
-static String mapInclusionSNOMED(final String inclusion) {
+static String[] getSnomedFields(final String inclusion) {
   switch (inclusion) {
     default:
       return null
     case "COV_JA":
-      return "373066001"
+      return ["373066001", "Yes (qualifier value)"]
     case "COV_NEIN":
-      return "373067005"
+      return ["373067005", "No (qualifier value)"]
     case "COV_UNBEKANNT":
-      return "261665006"
+      return ["261665006", "Unknown (qualifier value)"]
+    case "COV_NA":
+      return ["385432009", "Not applicable (qualifier value)"]
   }
 }
