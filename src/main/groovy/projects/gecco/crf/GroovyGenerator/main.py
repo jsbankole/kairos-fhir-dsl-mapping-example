3cce3b54d3c9410f806e1a483e633cfa6d64105e
Mario <macedo@medinfo.uni-kiel.de>
2022-03-24 15:24:14 +0100

    Main deletes all files from crf (not folders)

--- src/main/groovy/projects/gecco/crf/GroovyGenerator/main.py
+++ src/main/groovy/projects/gecco/crf/GroovyGenerator/main.py
@@ -9,15 +9,12 @@ def src(keyword):
 
 dest = "."
 
-# Delete old files in crf
+# Delete old files in crf (not folders)
 for filename in os.listdir(dest):
     file_path = os.path.join(dest, filename)
-
     try:
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            shutil.rmtree(file_path)
     except Exception as e:
         print('Failed to delete %s. Reason: %s' % (file_path, e))
 
